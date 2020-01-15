b8495e2da0c85d6584ec8655cc45deaa018fc167
Sophia Yoo <sophiayoo@temple.edu>
2020-01-15 16:43:17 -0500

    added multiple file capability

--- initial-utilities/wcat/wcat.c
+++ initial-utilities/wcat/wcat.c
@@ -8,27 +8,39 @@ wcat file
 
 
 
-int main(){
-    char filename[100];
-    scanf("%s", filename);
-    FILE *fp = fopen(filename, "r");
-    char c;
-
-    if(fp == NULL){
-        printf("wcat: cannot open file\n");
+int main(int argc, char *argv[]){
+    //exit program if a file name is not entered
+    if(argc <= 1){
+        printf("wcat: no file entered\n");
         exit(1);
     }
-    else{
-        //read contents from file
-        c = fgetc(fp);
-        //while there is more to the file
-        while(c != EOF){
-            //print the contents of the file
-            printf("%c", c);
-            c = fgetc(fp);
+
+    int index = 1;
+    while(index < argc){
+        //open filename given by argv at index argc
+        FILE *fp = fopen(argv[index], "r");
+        char c;
+
+        //check if file exists
+        if(fp == NULL){
+            printf("wcat: cannot open file\n");
+            exit(1);
         }
-        //file has been printed
-        fclose(fp);
+
+        else{
+            //read contents from file
+            c = fgetc(fp);
+            //while there is more to the file
+            while(c != EOF){
+                //print the contents of the file
+                printf("%c", c);
+                c = fgetc(fp);
+            }
+            //file has been printed
+            fclose(fp);
+        }
+        //increment index counter
+        index++;
     }
     //reach if program successful
     return 0;
